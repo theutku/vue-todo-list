5a5805c2665c52b8e495d8fd550b7d5f9a67422b
Utku Turkoglu <utkut@comodo.net>
2019-03-14 11:51:15 +0300

    fixed methods

--- www/src/store.js
+++ www/src/store.js
@@ -22,17 +22,20 @@ export default new Vuex.Store({
         SET_USER: (state, user) => {
             state.currentUser = user;
         },
-        REMOVE_USER: (state, user) => {
+        REMOVE_USER: (state) => {
             state.currentUser = user;
         }
     },
     actions: {
         login: (context, loginModel) => {
-            return axios.post(config.loginUrl, loginModel).then((user) => {
-                context.commit('SET_USER', user);
+            return axios.post(config.loginUrl, loginModel).then((res) => {
+                context.commit('SET_USER', res.data);
             }).catch((err) => {
-                context.commit('SHOW_ERROR', err);
+                context.commit('SHOW_ERROR', err || 'An unknown error has occured.');
             });
+        },
+        logout: ({ commit }) => {
+            commit('REMOVE_USER');
         }
     }
 });
